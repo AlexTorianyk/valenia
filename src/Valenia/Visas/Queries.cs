22585588352deda34ff868576bc319eac9fd3868
Alex Toryanik <alex1toryanik@gmail.com>
2021-01-08 22:32:11 +0100

    fix: change queries to ravendb implementation

--- src/Valenia/Visas/Queries.cs
+++ src/Valenia/Visas/Queries.cs
@@ -1,1 +1,71 @@
-﻿//
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Linq;
+using Raven.Client.Documents.Session;
+using Valenia.Domain.Visas;
+
+namespace Valenia.Visas
+{
+    public static class Queries
+    {
+        public static Task<List<ReadModels.VisaDetails>> Query(
+            this IAsyncDocumentSession session,
+            QueryModels.GetVisaDetails query
+        )
+        {
+            var lol = session.Query<Visa>()
+                .Where(x => x.Id.Value == query.VisaId)
+                .Select(
+                    x =>
+                        new ReadModels.VisaDetails
+                        {
+                            VisaId = x.Id.Value,
+                            Goal = x.Goal.Value,
+                            Type = x.Type,
+                            ExpectedProcessingTime = x.ExpectedProcessingTime.Days
+                        }
+                ).ToListAsync();
+
+            return lol;
+        }
+        public static Task<List<ReadModels.VisaGoal>> Query(
+            this IAsyncDocumentSession session,
+            QueryModels.GetVisaGoalsByType query
+        ) =>
+            session.Query<Visa>()
+                .Where(x => x.Type == query.Type)
+                .Select(
+                    x =>
+                        new ReadModels.VisaGoal
+                        {
+                            VisaId = x.Id.Value,
+                            Goal = x.Goal.Value
+                        }
+                ).PagedList(query.Page, query.PageSize);
+
+        public static Task<List<ReadModels.RequirementsDetails>> Query(
+            this IAsyncDocumentSession session,
+            QueryModels.GetVisaRequirements query
+        ) =>
+            session.Query<Visa>()
+                .Where(x => x.Id.Value == query.VisaId)
+                .Select(
+                    x =>
+                        new ReadModels.RequirementsDetails
+                        {
+                            VisaId = x.Id.Value,
+                            ExpectedProcessingTime = x.ExpectedProcessingTime.Days,
+                            Requirements = x.Requirements
+                        }
+                ).ToListAsync();
+
+        private static Task<List<T>> PagedList<T>(
+            this IRavenQueryable<T> query, int page, int pageSize
+        ) =>
+            query
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+    }
+}
--- src/Valenia/Visas/VisaQueryApi.cs
+++ src/Valenia/Visas/VisaQueryApi.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Raven.Client.Documents.Session;
+using Serilog;
+using Valenia.Infrastructure.Application;
+
+namespace Valenia.Visas
+{
+    [Route("/visa")]
+    public class VisaQueryApi : Controller
+    {
+        private static readonly ILogger _log = Log.ForContext<VisaQueryApi>();
+
+        private readonly IAsyncDocumentSession _session;
+
+        public VisaQueryApi(IAsyncDocumentSession session)
+            => _session = session;
+
+        [HttpGet]
+        public Task<IActionResult> Get(QueryModels.GetVisaDetails request)
+            => RequestHandler.HandleQuery(() => _session.Query(request), _log);
+
+        [HttpGet]
+        [Route("goals")]
+        public Task<IActionResult> Get(QueryModels.GetVisaGoalsByType request)
+            => RequestHandler.HandleQuery(() => _session.Query(request), _log);
+
+
+        [HttpGet]
+        [Route("requirements")]
+        public Task<IActionResult> Get(QueryModels.GetVisaRequirements request)
+            => RequestHandler.HandleQuery(() => _session.Query(request), _log);
+    }
+}
