ac708b940cc74264babad3c50da7aacdf4b47895
Alex Toryanik <alex1toryanik@gmail.com>
2021-01-24 12:47:01 +0100

    feature: application infrastructure classes

---
+++ src/Valenia/Applications/ApplicationCommands.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Valenia.Applications
+{
+    public static class ApplicationCommands
+    {
+        public class Submit
+        {
+            public Guid ApplicantId { get; set; }
+            public Guid VisaId { get; set; }
+            public DateTimeOffset SubmissionDate { get; set; }
+        }
+
+        public class AddDocument
+        {
+            public Guid Id { get; set; }
+            public string DocumentUrl { get; set; }
+        }
+
+        public class AssignToReviewer
+        {
+            public Guid Id { get; set; }
+            public Guid ReviewerId { get; set; }
+        }
+
+        public class RequestChanges
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Approve
+        {
+            public Guid Id { get; set; }
+        }
+    }
+}
